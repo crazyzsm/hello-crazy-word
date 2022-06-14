0453d3bf51d0c6c0329e8f0a419d4a4d1649b7d3
crazyzsm <43157254+crazyzsm@users.noreply.github.com>
2022-06-14 11:53:59 +0800

    Create test.vue
    test

---
+++ test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" style="width:100vw;height:100vh">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+div{
+  box-sizing: border-box;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
